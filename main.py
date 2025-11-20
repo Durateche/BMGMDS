ebb15dec645dd9d94409c86a236e0826e913c24b
Durateche <greshilov1312@gmail.com>
2025-11-20 21:31:26 +0300

    main.py

--- main.py
+++ main.py
@@ -1,31 +1,114 @@
-edit_message_text(
-                f"Страны ({name}):",
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
+
+# Global variables
+user_data = {}
+DB = {}  # Ваша база данных с континентами, странами и марками
+
+# Функция для создания клавиатуры с пагинацией
+def paginated_keyboard(items, page, back=None):
+    # Ваша реализация пагинации
+    pass
+
+# Функция start
+async def start(update, context):
+    # Ваша реализация команды /start
+    pass
+
+async def callback(update, context):
+    query = update.callback_query
+    await query.answer()
+    
+    user_id = query.from_user.id
+    data = query.data
+    
+    # Initialize user data if not exists
+    if user_id not in user_data:
+        user_data[user_id] = {"level": "continent", "continent": "", "country": "", "page": 0}
+    
+    user = user_data[user_id]
+    
+    # Handle pagination
+    if data.startswith("page_"):
+        page = int(data.split("_")[1])
+        user["page"] = page
+        
+        if user["level"] == "continent":
+            continents = list(DB.keys())
+            return await query.edit_message_text(
+                "Выберите континент:",
+                reply_markup=paginated_keyboard(continents, page, back=None)
+            )
+        elif user["level"] == "country":
+            cont = user["continent"]
+            countries = list(DB[cont].keys())
+            return await query.edit_message_text(
+                f"Страны ({cont}):",
+                reply_markup=paginated_keyboard(countries, page, back="back_cont")
+            )
+        elif user["level"] == "brand":
+            cont = user["continent"]
+            cnt = user["country"]
+            brands = list(DB[cont][cnt].keys())
+            return await query.edit_message_text(
+                f"Марки ({cnt}):",
+                reply_markup=paginated_keyboard(brands, page, back="back_country")
+            )
+    
+    # Handle back buttons
+    if data == "back_cont":
+        user["level"] = "continent"
+        user["continent"] = ""
+        user["page"] = 0
+        continents = list(DB.keys())
+        return await query.edit_message_text(
+            "Выберите континент:",
+            reply_markup=paginated_keyboard(continents, 0, back=None)
+        )
+    elif data == "back_country":
+        user["level"] = "country"
+        user["country"] = ""
+        user["page"] = 0
+        cont = user["continent"]
+        countries = list(DB[cont].keys())
+        return await query.edit_message_text(
+            f"Страны ({cont}):",
+            reply_markup=paginated_keyboard(countries, 0, back="back_cont")
+        )
+    
+    # Handle continent selection
+    if user["level"] == "continent":
+        if data in DB:
+            user["level"] = "country"
+            user["continent"] = data
+            user["page"] = 0
+            countries = list(DB[data].keys())
+            return await query.edit_message_text(
+                f"Страны ({data}):",
                 reply_markup=paginated_keyboard(countries, 0, back="back_cont")
             )
-
-        # Level: country
-        if user["level"]=="country":
-        cont=user["continent"]
-            if name in DB[cont]:
-            user["level"]="brand"
-            user["country"]=name
-            user["page"]=0
-
-                brands=list(DB[cont][name].keys())
-                return await query.edit_message_text(
-                    f"Марки ({name}):",
-                    reply_markup=paginated_keyboard(brands, 0, back="back_country")
-                )
-
-        # Level: brand
-        if user["level"]==brand":
+    
+    # Handle country selection
+    elif user["level"] == "country":
+        cont = user["continent"]
+        if data in DB[cont]:
+            user["level"] = "brand"
+            user["country"] = data
+            user["page"] = 0
+            brands = list(DB[cont][data].keys())
+            return await query.edit_message_text(
+                f"Марки ({data}):",
+                reply_markup=paginated_keyboard(brands, 0, back="back_country")
+            )
+    
+    # Handle brand selection
+    elif user["level"] == "brand":
         cont = user["continent"]
         cnt = user["country"]
-            if name in DB[cont][cnt]:
-            link = DB[cont][cnt][name]
-                return await query.edit_message_text(
-                    f"Марка: {name}\nСайт: {link}"
-                )
+        if data in DB[cont][cnt]:
+            link = DB[cont][cnt][data]
+            return await query.edit_message_text(
+                f"Марка: {data}\nСайт: {link}"
+            )
 
 
 async def main():
@@ -38,5 +121,5 @@ async def main():
 
 
 if name == "__main__":
-import asyncio
-asyncio.run(main())
+    import asyncio
+    asyncio.run(main())
