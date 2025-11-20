13d162df91ffa6fd10df6d3ca4d1dc700e705399
Durateche <greshilov1312@gmail.com>
2025-11-20 21:01:59 +0300

    main.py

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ edit_message_text(
             )
 
         # Level: country
-        if user["level"] == "country":
-        cont = user["continent"]
+        if user["level"]=="country":
+        cont=user["continent"]
             if name in DB[cont]:
-            user["level"] = "brand"
-            user["country"] = name
-            user["page"] = 0
+            user["level"]="brand"
+            user["country"]=name
+            user["page"]=0
 
-                brands = list(DB[cont][name].keys())
+                brands=list(DB[cont][name].keys())
                 return await query.edit_message_text(
                     f"Марки ({name}):",
                     reply_markup=paginated_keyboard(brands, 0, back="back_country")
                 )
 
         # Level: brand
-        if user["level"] == "brand":
+        if user["level"]==brand":
         cont = user["continent"]
         cnt = user["country"]
             if name in DB[cont][cnt]:
