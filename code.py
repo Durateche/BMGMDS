c5fd7cb1faaf28208d74719f5d55dbfd7e70f741
Durateche <greshilov1312@gmail.com>
2025-11-20 20:31:14 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,42 @@
+edit_message_text(
+                f"Страны ({name}):",
+                reply_markup=paginated_keyboard(countries, 0, back="back_cont")
+            )
+
+        # Level: country
+        if user["level"] == "country":
+            cont = user["continent"]
+            if name in DB[cont]:
+                user["level"] = "brand"
+                user["country"] = name
+                user["page"] = 0
+
+                brands = list(DB[cont][name].keys())
+                return await query.edit_message_text(
+                    f"Марки ({name}):",
+                    reply_markup=paginated_keyboard(brands, 0, back="back_country")
+                )
+
+        # Level: brand
+        if user["level"] == "brand":
+            cont = user["continent"]
+            cnt = user["country"]
+            if name in DB[cont][cnt]:
+                link = DB[cont][cnt][name]
+                return await query.edit_message_text(
+                    f"Марка: {name}\nСайт: {link}"
+                )
+
+
+async def main():
+    app = ApplicationBuilder().token(8201961290:AAHW76Rh21ArYey8um56d2R4yDPgFvFWal4).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(callback))
+
+    await app.run_polling()
+
+
+if name == "__main__":
+    import asyncio
+    asyncio.run(main())
